ecb6be4b8f3c86822570c868bab1e04c8ac69d6b
akitobora <akitobora@gmail.com>
2022-04-30 15:40:23 +0300

    Add JS siles

---
+++ lesson_1.js
@@ -0,0 +1,6 @@
+let yolochka4 = 4;
+let yolochka6 = 6;
+
+let result = yolochka4 + yolochka6;
+console.log(result);
+console.log("Hello");
---
+++ let t = true.js
@@ -0,0 +1,20 @@
+let age = 10;
+let tt = typeof(age)
+console.log(tt)
+
+
+
+if (typeof(age)=="number"){
+if (age >= 18 && age < 60){
+    console.log("Full content")
+}
+else if (age<18){
+    console.log("Miltiki")
+}
+else if (age>=60){
+    console.log("Kultura")
+} else {
+    console.log("Good luck!")
+} else { 
+    console.log("Bad")} 
+}
